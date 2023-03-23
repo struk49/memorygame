f844e9f6e4ff7695fa7abc2fa0b41188652bd996
struk49 <andrewstruk@yahoo.co.uk>
2023-03-23 13:25:27 +0000

    have no clue

--- game.js
+++ game.js
@@ -1,18 +1,18 @@
 const cardArray = [
     {
         name: 'fries',
-        img: 'images/fries.png',  
+        img: 'images/fries(1).png',  
     },
     {
         name: 'cheeseburger',
-        img: 'images/cheeseburger.png',
+        img: 'images/cheeseburger(1).png',
     },
     {
         name: 'hotdog',
-        img: 'images/hotdog.png',
+        img: 'images/hotdog(1).png',
     },
     {
-        name: 'icecream',
+        name: 'ice-cream',
         img: 'images/ice-cream.png',
     },
     {
@@ -48,18 +48,24 @@ const cardArray = [
        img: 'image/pizza.png' 
     }
 ]
-cardArray.sort(() => 0,5 - Math.random())
+cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
  
 function createBoard() {
-    for (let i = 0; i < 10; 1++) {
+    for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
-      card.setAttribute('src', '/imags/blank.png')
+      card.setAttribute('src', '/images/blank.png')
       card.setAttribute('data-id', i)
-      gridDisplay.append(card)
+      card.addEventListener('click', flipCard())
+      gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    console.log('clicked', cardId);
+}
 
